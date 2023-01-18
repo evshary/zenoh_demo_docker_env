f1fda10a07d143eba707a99d6b1b2ac14431ec92
ChenYing Kuo <evshary@gmail.com>
2023-01-18 11:45:27 +0800

    Use integrated GPU to run Autoware.

--- autoware_multiple_teleop/run.sh
+++ autoware_multiple_teleop/run.sh
@@ -2,7 +2,7 @@
 
 xhost +
 docker run --rm -it --privileged \
-    --env "LIBGL_ALWAYS_SOFTWARE=1" --env "ROS_LOCALHOST_ONLY=1" -v $PWD/autoware.sh:/root/autoware.sh \
+    --env "ROS_LOCALHOST_ONLY=1" -v $PWD/autoware.sh:/root/autoware.sh \
     -v $PWD/myconfig.json5:/root/myconfig.json5 \
     -v /tmp/.X11-unix:/tmp/.X11-unix --env="QT_X11_NO_MITSHM=1" --env "DISPLAY=$DISPLAY" \
     autoware-bridge bash
